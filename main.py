b1de0d399df7e9acd04e5d288875c4b9a36fa4ca
Egor Neznaev <01neznaevegor@gmail.com>
2024-03-08 17:37:54 +0300

    first

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+pygame.init()
+screen = pygame.display.set_mode((1500, 600))
+pygame.display.set_caption("Слово пацана")
+icon = pygame.image.load('images/icon.png')
+pygame.display.set_icon(icon)
+
+running = True
+while running:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
